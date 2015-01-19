b62a81beb275bae794d3dcfbdaa750f6d1104fdc
jaxgeller <jacksongeller@gmail.com>
2015-01-19 17:31:02 -0500

    redo jade scss, although stil not happy with layout, good on 1280-1400, okay on bigger screens, no responsive yet.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getTemplate(isogram) {
-  var template = _.template("&lt;script&gt;<br/>&nbsp;&nbsp;(function(<%=isogram[0]%>,<%=isogram[1]%>,<%=isogram[2]%>,<%=isogram[3]%>,<%=isogram[4]%>,<%=isogram[5]%>,<%=isogram[6]%>){<%=isogram[0]%>[&#039;GoogleAnalyticsObject&#039;]=<%=isogram[4]%>;<%=isogram[0]%>[<%=isogram[4]%>]=<%=isogram[0]%>[<%=isogram[4]%>]||function(){<br />&nbsp;&nbsp;(<%=isogram[0]%>[<%=isogram[4]%>].q=<%=isogram[0]%>[<%=isogram[4]%>].q||[]).push(arguments)},<%=isogram[0]%>[r].l=1*new Date();<%=isogram[5]%>= <%=isogram[1]%>.createElement(<%=isogram[2]%>),<br />&nbsp;&nbsp;<%=isogram[6]%>= <%=isogram[1]%>.getElementsByTagName(<%=isogram[2]%>)[0];<%=isogram[5]%>.async=1;<%=isogram[5]%>.src=<%=isogram[3]%> ;<%=isogram[6]%>.parentNode.insertBefore(<%=isogram[5]%>,<%=isogram[6]%>)<br />&nbsp;&nbsp;})(window,document,&#039;script&#039;,&#039;//www.google-analytics.com/analytics.js&#039;,&#039;ga&#039;);<br /><br />&nbsp;&nbsp;ga(&#039;create&#039;, &#039;UIDHERE&#039;, &#039;auto&#039;);<br />&nbsp;&nbsp;ga(&#039;send&#039;, &#039;pageview&#039;);<br /><br />&lt;/script&gt;");
+  var template = _.template("&lt;script&gt;<br/>&nbsp;&nbsp;(function(<%=isogram[0]%>,<%=isogram[1]%>,<%=isogram[2]%>,<%=isogram[3]%>,<%=isogram[4]%>,<%=isogram[5]%>,<%=isogram[6]%>){<%=isogram[0]%>[&#039;GoogleAnalyticsObject&#039;]=<%=isogram[4]%>;<%=isogram[0]%>[<%=isogram[4]%>]=<%=isogram[0]%>[<%=isogram[4]%>]||function(){<br />&nbsp;&nbsp;(<%=isogram[0]%>[<%=isogram[4]%>].q=<%=isogram[0]%>[<%=isogram[4]%>].q||[]).push(arguments)},<%=isogram[0]%>[r].l=1*new Date();<%=isogram[5]%>= <%=isogram[1]%>.createElement(<%=isogram[2]%>),<br />&nbsp;&nbsp;<%=isogram[6]%>= <%=isogram[1]%>.getElementsByTagName(<%=isogram[2]%>)[0];<%=isogram[5]%>.async=1;<%=isogram[5]%>.src=<%=isogram[3]%>;<%=isogram[6]%>.parentNode.insertBefore(<%=isogram[5]%>,<%=isogram[6]%>)<br />&nbsp;&nbsp;})(window,document,&#039;script&#039;,&#039;//www.google-analytics.com/analytics.js&#039;,&#039;ga&#039;);<br /><br />&nbsp;&nbsp;ga(&#039;create&#039;, &#039;UIDHERE&#039;, &#039;auto&#039;);<br />&nbsp;&nbsp;ga(&#039;send&#039;, &#039;pageview&#039;);<br /><br />&lt;/script&gt;");
 
   return template({isogram: isogram});
 }
@@ -31,34 +31,38 @@ function isIsogram(word){
 
 
 $(document).on('ready', function() {
-  var $input = $('.isogram-input');
+  $('.output-bottom').html(getTemplate('isogram'.split('')))
+  $('.output-bottom').click(function() {
+    $('.output-bottom').select();
+  })
+  // var $input = $('.isogram-input');
 
-  $input.on('input', function() {
+  // $input.on('input', function() {
 
-    if ($input.val().length === 7) {
+  //   if ($input.val().length === 7) {
 
-      if (isIsogram($input.val())) {
-        $input.removeClass('invalid');
-        $input.addClass('valid');
-        var split = $input.val().split('');
+  //     if (isIsogram($input.val())) {
+  //       $input.removeClass('invalid');
+  //       $input.addClass('valid');
+  //       var split = $input.val().split('');
 
-        $('.isogram-code').html(getTemplate(split));
-      }
-      else {
-        $input.removeClass('valid');
-        $input.addClass('invalid');
-      }
-    }
+  //       $('.isogram-code').html(getTemplate(split));
+  //     }
+  //     else {
+  //       $input.removeClass('valid');
+  //       $input.addClass('invalid');
+  //     }
+  //   }
 
-    else {
-      $input.removeClass('valid');
-      $input.addClass('invalid');
-    }
+  //   else {
+  //     $input.removeClass('valid');
+  //     $input.addClass('invalid');
+  //   }
 
-    if ($input.val().length <= 0) {
-      $input.removeClass('valid');
-      $input.removeClass('invalid');
-    }
-  });
+  //   if ($input.val().length <= 0) {
+  //     $input.removeClass('valid');
+  //     $input.removeClass('invalid');
+  //   }
+  // });
 
 });
